cc835e957b6d178b91b4191d0e4f02d1fc2284fe
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2025-01-22 14:21:16 -0300

    Update Ninja.java

--- src/main/java/ecrescer/Ninja.java
+++ src/main/java/ecrescer/Ninja.java
@@ -1,27 +1,43 @@
 package ecrescer;
 
 public class Ninja {
+
     private String nome;
-    private int chakra;
     private Jutsu jutsu;
+    private Integer chakra;
+
     public Ninja(String nome, Jutsu jutsu) {
         this.nome = nome;
         this.jutsu = jutsu;
         this.chakra = 100;
     }
+
     public String getNome() {
         return nome;
     }
-    public int getChakra() {
-        return chakra;
-    }
-    public void setChakra(int chakra) {
-        this.chakra = chakra;
-    }
-    public void atacar(Ninja oponente) {
-        oponente.chakra -= jutsu.getDano();
-    }
+
     public Jutsu getJutsu() {
         return jutsu;
+    }
+
+    public Integer getChakra() {
+        return chakra;
+    }
+
+    public void setChakra(Integer chakra) {
+        this.chakra = chakra;
+    }
+
+    public void atacar(Ninja oponente) {
+        if (this.jutsu == null || oponente == null || oponente.getChakra() == null || this.chakra == null || this.chakra < 0) {
+            return;
+        }
+
+        int dano = this.jutsu.getDano();
+
+        if (oponente.getChakra() > 0) {
+            int novoChakra = oponente.getChakra() - dano;
+            oponente.setChakra(Math.max(novoChakra, 0));
+        }
     }
 }
